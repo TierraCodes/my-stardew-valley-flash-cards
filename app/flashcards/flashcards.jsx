7cd28870821c4bcc6cce5b1c1a2587bd2b5deb48
TierraCodes <112350897+TierraCodes@users.noreply.github.com>
2024-11-22 11:05:46 -0500

    attempt at making the buttons update on click

--- app/flashcards/flashcards.jsx
+++ app/flashcards/flashcards.jsx
@@ -3,10 +3,26 @@ import { useState } from "react";
 import { useEffect } from "react";
 
 export default function Flashcards(props){
+    let [percentUnderstood, setPercentUnderstood] = useState(1)
+
+    const updatePercentUnderstood = (button) =>{
+        let percent = 1
+        if(percentUnderstood <= 100){
+            if(button==="easy"){
+                percent = (percentUnderstood + (percentUnderstood * 25))
+            }else if(button==="medium"){
+                percent = (percentUnderstood + (percentUnderstood * 15))
+            }else if(button==="hard"){
+                percent = (percentUnderstood + (percentUnderstood * 3))
+            }
+        }
+        setPercentUnderstood(percent)
+    }
 
     const getRandomCharacter = () =>{
         const index = Math.floor(Math.random()*props.villagers.length)
         setCurrentVillager(props.villagers[index])
+        setAnswer(false)
     }
 
     const [currentVillager,setCurrentVillager] = useState([])
@@ -15,17 +31,41 @@ export default function Flashcards(props){
         getRandomCharacter()
     }, [])
 
+    const showAnswer = () =>{
+        setAnswer(true)
+    }
+
+    const [answer,setAnswer] = useState(false)
+
+    const villagerBirthday = 
+        <div className="flashcard" key={currentVillager.id}
+            onClick={()=> getRandomCharacter()}>
+            {currentVillager.name}'s birthday is: 
+            <br></br>
+            {currentVillager.birthday}
+        </div>
+
     const villagerCard = 
         <div className="flashcard" key={currentVillager.id} 
-            onClick={()=> getRandomCharacter()}>
+            onClick={()=> showAnswer()}>
             {currentVillager.name}
-            <img src={currentVillager.pic} alt="" />
+            <img src={currentVillager.pic} alt="Official image of villager" />
             {currentVillager.question}
         </div>
 
     return(
         <div id='flashcards-page'>
-            {villagerCard}
+            <div>
+                {answer ? villagerBirthday : villagerCard }
+            </div>
+            <div id="difficulty-buttons">
+                <div>
+                    <button id="easy-button" onClick={updatePercentUnderstood("easy")}>Easy</button>
+                    <button id="medium-button" onClick={updatePercentUnderstood("medium")}>Medium</button>
+                    <button id="hard-button" onClick={updatePercentUnderstood("hard")}>Hard</button>
+                </div>
+                <div>How hard is this question?</div>
+            </div>
         </div>
     )
 }
--- app/villagers/villagersList.jsx
+++ app/villagers/villagersList.jsx
@@ -1,19 +1,32 @@
 'use client'
 import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 
 
 export default function villagerList(props){
     const router = useRouter()
 
+    const percentUnderstood = 0
+
+    let [villagerInfo, setVillagerInfo] = useState(-1)
+
     const villagerList = props.villagers.map((villager)=>
-        <li key={villager.id} className='villager-list'>
+        <li key={villager.id} className='villager-list' onClick={setVillagerInfo(villager.id)}>
             <div id="villager-header">
                 <span id='villager-name'>{villager.name}</span>
-                <span id='percentage-learned'>%</span>
+                <span id='percentage-learned'>{percentUnderstood}%</span>
             </div>
             <div>
                 <img src={villager.pic}/>
             </div>
+            <span style={{display: villagerInfo == villager.id ? "block" : "none"}}>
+                <div>
+                    <br></br>
+                    {villager.birthday}
+                    <br></br>
+                    {villager.relationshipStatus}
+                </div>
+            </span>
         </li>
     )
 
